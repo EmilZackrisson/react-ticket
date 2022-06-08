816fc2c0695205020b8973996ed8da590287c99b
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-06-08 23:48:20 +0200

    ändrad todo

--- client/src/todo.js
+++ client/src/todo.js
@@ -6,11 +6,11 @@ function todo() {
             {/* <h3>Lägg till delete, id undefined</h3> */}
             <h1>Todo</h1>
             {/* <h5>klar status visa</h5> */}
-            <h5>fixa env för discord och db</h5>
+            {/* <h5>fixa env för discord och db</h5> */}
             <h5>Fixa Styling</h5>
             <h5>React bootstrap darkmode</h5>
             <h5>Inloggning</h5>
-            <h5>Inställningar som skriver till fil .env</h5>
+            <h5>Inställningar för att konfigurera db, mail och discord som skriver till fil</h5>
             <h5>Fixa mail</h5>
             {/* <h5>fixa discord</h5> */}
         </div>
