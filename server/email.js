dc576b47095473706f21f36c329dc30215a9eda1
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-09-15 19:46:08 +0200

    fix issue email

--- server/email.js
+++ server/email.js
@@ -7,7 +7,7 @@ function sendNewIssue(issueObj) {
 
     const name = issueObj.senderName;
     const email = issueObj.senderEmail;
-    const issue = issueObj.issue;
+    const issue = issueObj.issue.issue;
 
     const id = issueObj.id;
     const subject = "Nytt problem - #" + id;
@@ -60,7 +60,7 @@ function sendNewIssue(issueObj) {
 function issueSolved(issueObj){
     const name = issueObj.senderName;
     const email = issueObj.senderEmail;
-    const issue = issueObj.issue;
+    const issue = issueObj.issue.issue;
     const id = issueObj.id;
     const subject = "Nytt problem - #" + id;
     const content = "<body><h1>Problem löst #" + id + "</h1><h3>Rapporterad av " + name + " | <a href:mailto=" + email + ">" + email + "</a></h3><p>" + issue + "</p></body>"
@@ -112,7 +112,7 @@ function issueSolved(issueObj){
 function issueChanged(issueObj){
     const name = issueObj.senderName;
     const email = issueObj.senderEmail;
-    const issue = issueObj.issue;
+    const issue = issueObj.issue.issue;
     const id = issueObj.id;
     const subject = "Nytt problem - #" + id;
     const content = "<body><h1>Problem uppdaterat #" + id + "</h1><h3>Rapporterad av " + name + " | <a href:mailto=" + email + ">" + email + "</a></h3><p>" + issue + "</p></body>"
